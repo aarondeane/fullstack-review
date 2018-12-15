5eb4faa69bbcaaf6c1c33283d7bd7c0f7d86d0db
Aaron Deane <aaronmdeane@gmail.com>
2018-12-14 20:05:10 -0800

    Dynamically rendering done

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const RepoList = (props) => (
+const RepoList = (props) => {
+  const repoList = props.repos.map(repo =>
+    <div>User:{repo.user}<br />
+         Repo:{repo.name}</div>
+    );
+  
+  return(  
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    <div>{repoList}</div>
   </div>
-)
+  );
+}
 
 export default RepoList;
